caf4b5396ea32d3e77889b5724140b8d47d166a7
hackleman <hackleman.jason@gmail.com>
2019-02-23 16:30:14 -0600

    changed connection string to use SID instead of instance
    added index page to test database connection

--- services/database.js
+++ services/database.js
@@ -6,15 +6,42 @@ async function initialize() {
 
 //  const pool = await oracledb.createPool(dbConfig.CISEPool);
 
-oracledb.getConnection(
-  {
-    user          :     process.env.CISE_USER,
-    password      :     process.env.CISE_PASSWORD,
-    connectString :     process.env.CISE_CONNECTIONSTRING
-  })
+await oracledb.createPool(dbConfig.CISEPool);
 
   console.log("Oracle DB Connected~!!!");
 
 }
 
+async function close() {
+  await oracledb.getPool().close();
+}
+
+function simpleExecute(statement, binds = [], opts = {}) {
+  return new Promise(async (resolve, reject) => {
+    let conn;
+
+    opts.outFormat = oracledb.OBJECT;
+    opts.autoCommit = true;
+
+    try {
+      conn = await oracledb.getConnection();
+
+      const result = await conn.execute(statement, binds, opts);
+
+      resolve(result);
+    } catch (err) {
+      reject(err);
+    } finally {
+      if (conn) { // conn assignment worked, need to close
+        try {
+          await conn.close();
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    }
+  });
+}
+module.exports.simpleExecute = simpleExecute;
+module.exports.close = close;
 module.exports.initialize = initialize;
--- services/web-server.js
+++ services/web-server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const express = require('express');
 const morgan = require('morgan');
 const webServerConfig = require('../config/web-server.js');
+const database = require('./database.js');
 
 let httpServer;
 
@@ -11,8 +12,12 @@ function initialize() {
     httpServer = http.createServer(app);
 
     app.use(morgan('combined'));
-    app.get('/', (req, res) => {
-      res.end('Hello World!');
+    app.get('/', async (req, res) => {
+      const result = await database.simpleExecute('select user, systimestamp from dual');
+      const user = result.rows[0].USER;
+      const date = result.rows[0].SYSTIMESTAMP;
+
+      res.end(`DB user: ${user}\nDate: ${date}`);
     });
 
     httpServer.listen(webServerConfig.port)
